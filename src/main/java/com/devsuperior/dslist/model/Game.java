3c5db4ddf7276c06f376cf49d8597860e2207bba
LucasAndrade1605 <lucascarlo2003@gmail.com>
2025-05-19 23:44:16 -0400

    model e ORM Game

---
+++ src/main/java/com/devsuperior/dslist/model/Game.java
@@ -0,0 +1,50 @@
+package com.devsuperior.dslist.model;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_game")
+public class Game {
+	
+	 	@Id
+	    @GeneratedValue
+	    @Column(nullable = false)
+	    private UUID id;
+
+	    @Column(nullable = false, length = 100)
+	    private String titulo;
+
+	    @Column(nullable = false)
+	    private Integer ano;
+
+	    @Column(length = 50)
+	    private String genero;
+
+	    @Column(length = 50)
+	    private String plataforma;
+
+	    private Double score;
+
+	    @Column(name = "img_url", length = 255)
+	    private String imgUrl;
+
+	    @Column(name = "descricao_curta", length = 500)
+	    private String descricaoCurta;
+
+	    @Column(name = "descricao_longa", columnDefinition = "TEXT")
+	    private String descricaoLonga;
+	
+}
